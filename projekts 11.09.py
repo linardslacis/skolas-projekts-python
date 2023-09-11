e1b0a5c22ac9a1299a1a84be937312b3454825af
Linards.Lacis <llacis2005@gmail.com>
2023-09-11 13:43:01 +0300

    Initilize project structure

---
+++ projekts 11.09.py
@@ -0,0 +1,12 @@
+import matplotlib
+
+print(matplotlib.__version__)
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+xpoints = np.array([0, 6, 20])
+ypoints = np.array([0, 250, 300])
+
+plt.plot(xpoints, ypoints)
+plt.show()
