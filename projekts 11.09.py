11aeb23aaf92c5d9e2c952efcde2b3ef55cb4909
linardslacis <144676028+linardslacis@users.noreply.github.com>
2023-09-11 13:57:33 +0300

    Update projekts 11.09.py

--- projekts 11.09.py
+++ projekts 11.09.py
@@ -5,7 +5,7 @@ print(matplotlib.__version__)
 
 import matplotlib.pyplot as plt
 import numpy as np
-xpoints = np.array([0, 6, 20])
+xpoints = np.array([0, 7, 20])
 ypoints = np.array([0, 250, 300])
 
 
