be37cb7f028561459a7e3716471212856c72f5ac
Linards.Lacis <llacis2005@gmail.com>
2023-10-02 13:19:43 +0300

    Initilize project structure

--- projekts 11.09.py
+++ projekts 11.09.py
@@ -1,13 +1,22 @@
 import matplotlib
+import pandas
+import seaborn
+import numpy
 
 print(matplotlib.__version__)
 
-
 import matplotlib.pyplot as plt
 import numpy as np
-xpoints = np.array([0, 7, 20])
-ypoints = np.array([0, 250, 300])
+
+pieprasijumus1 = np.array([0, 7, 20])
+cena1 = np.array([0, 250, 300])
+
+piedavajums2 = np.array([10, 7, 20])
+cena2 = np.array([0, 250, 300])
 
 
-plt.plot(xpoints, ypoints)
+
+
+plt.plot(pieprasijumus1, cena1)
+plt.plot(piedavajums2, cena2)
 plt.show()
