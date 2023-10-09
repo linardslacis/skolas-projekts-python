c39d30c146f0eb3863d22bcd5a785f79cad32346
Linards.Lacis <llacis2005@gmail.com>
2023-10-09 14:01:55 +0300

    Initilize project structure

--- projekts 11.09.py
+++ projekts 11.09.py
@@ -15,29 +15,39 @@ y1 = float(input("Un tam atbilstošo cenu: "))
 #print("The entered number is:", x1)
 x2 = float(input("Ievadi pieprasījumu: "))
 y2 = float(input("Un tam atbilstošo cenu: "))
+x3 = float(input("Ievadi piedāvājumu: "))
+y3 = float(input("Un tam atbilstošo cenu: "))
+#print("The entered number is:", x1)
+x4 = float(input("Ievadi piedāvājumu: "))
+y4 = float(input("Un tam atbilstošo cenu: "))
 
-#float= z    uztaisit taisnes vienadojumu un pagarinat taisni
-#print(type(z))
-#input(z)
+
+b1 = float
+b2 = float
+k1 = float
+k2 = float
+def Pieprasijuma_likne(x1, y1, x2, y2):
+    k1=(y2-y1)/(x2-x1)
+    b1=y1-k1*x1
+    return f"y = {k1}x + {b1}"
+
+likne1 = Pieprasijuma_likne(x1, y1, x2, y2)
+print("Pieprasījuma līknes vienādojums",likne1)
+def Piedavajuma_likne(x3, y3, x4, y4):
+    k2=(y4-y3)/(x4-x3)
+    b2=y3-k2*x3
+    return f"y = {k2}x + {b2}"
+
+likne2 = Piedavajuma_likne(x3, y3, x4, y4)
+print("Piedāvājuma liknes vienādojums",likne2)
+xkr = (b2 - b1)/(k1 - k2)
+print("xkrus", xkr)
+
 
 pieprasijumus1 = np.array([x1, x2])
 cena1 = np.array([y1, y2])
-
-#jauns
-
-def Pieprasijuma_likne(x1, y1, x2, y2):
-k=(y2-y1)/(x2-x1)
-b1=y1-k*x1
-return f"y = {k}x + {b1}"
-likne1 = Pieprasijuma_likne(x1, y1, x2, y2)
-print("Pieprasijuma liknes vienadojums", )
-
-piedavajums2 = np.array([10, 7])
-cena2 = np.array([0, 250])
-
-
-
-
+piedavajums2 = np.array([x3, x4])
+cena2 = np.array([y3, y4])
 plt.plot(pieprasijumus1, cena1)
 plt.plot(piedavajums2, cena2)
 plt.show()
