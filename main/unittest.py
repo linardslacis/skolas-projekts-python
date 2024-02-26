4fbf5ee9835759c4120e0e3de3f18fc424844f60
Linards.Lacis <llacis2005@gmail.com>
2024-02-26 14:10:32 +0200

    Initilize project structure

--- main/unittest.py
+++ main/unittest.py
@@ -1,6 +1,6 @@
 # Vienibu testi japarsauc faili lai nebutu atstarpes
 import unittest
-from Algaskalkulators import parbaude
+from Algas_kalkulators import parbaude
 
 class TestAlgaskallkulators(unittest.TestCase):
 
@@ -8,8 +8,12 @@ class TestAlgaskallkulators(unittest.TestCase):
         self.assertEqual(parbaude(734,5))
         self.assertEqual(parbaude(-13,4))
         self.assertEqual(parbaude(12.5))
+        result = parbaude(-5)
+        expected = -5
+        self.assretEqual(result, expected)
 
-    def test_parbaude1(self):
-        result = test_parbaude1('input')
+
+    def test_parbaude(self):
+        result = test_parbaude('input')
         expected = 'expected output'
         self.assretEqual(result, expected)
